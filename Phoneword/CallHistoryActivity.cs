da194bbfc652dad57c30fa41d2f53c219f4f4acf
anatoleDevayr <amattei@devayr.com>
2015-12-01 18:20:20 -0300

    Comentrio tole

--- Phoneword/CallHistoryActivity.cs
+++ Phoneword/CallHistoryActivity.cs
@@ -22,7 +22,7 @@ namespace Phoneword
             // Create your application here
             var phoneNumbers = Intent.GetStringArrayListExtra("phone_numbers") ?? new string[0];
             this.ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, phoneNumbers);
-
+            //Comentario tole probar
         }
     }
 }
